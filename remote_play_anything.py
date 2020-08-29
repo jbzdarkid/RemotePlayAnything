7eb8e19edb7fec3b1477ba0001a4ac84b2cbe764
jbzdarkid <jbzdarkid@gmail.com>
2020-08-29 14:39:48 -0700

    A lot of work

--- remote_play_anything.py
+++ remote_play_anything.py
@@ -1,28 +1,37 @@
-import json
+import os
 import requests
 import shutil
 import subprocess
 import sys
 from pathlib import Path
 
-# This file is used to communicate information from this runtime until the next time we get called.
-restart_file = Path('C:/Users/localhost/Desktop/foo.txt')
+# This file is used to communicate information from this runtime into the steam invocation.
+restart_file = Path.home() / 'remote_play_anywhere.txt'
 
 steam_folder = None
 def get_steam_folder():
   global steam_folder
   if sys.platform == 'win32':
     import winreg
-    steam_folder = 'C:/Program Files (x86)/Steam'
     try:
       (steam_folder, _) = winreg.QueryValue(winreg.HKEY_CURRENT_USER, 'SOFTWARE\Valve\Steam\SteamPath')
+      steam_folder = Path(steam_folder)
     except OSError:
-      pass
-    print('Determined steam folder: ' + steam_folder)
+      steam_folder = Path('C:/Program Files (x86)') / 'Steam'
+  elif sys.platform == 'darwin':
+    steam_folder = Path.home() / 'Library' / 'Application Support' / 'Steam'
   else:
-    steam_folder = input('Enter steam folder: ')
+    steam_folder = Path(input('Enter steam folder: '))
+  print('Determined steam folder: ' + str(steam_folder))
 
-  steam_folder = Path(steam_folder)
+
+def open_url(url):
+  if sys.platform == 'win32':
+    subprocess.run(['cmd', '/c', 'start', url])
+  elif sys.platform == 'darwin':
+    os.command('open steam://rungameid/' + url)
+  else:
+    os.command('xdg-open ' + url)
 
 
 def show_chooser(options, key=None):
@@ -35,34 +44,44 @@ def show_chooser(options, key=None):
   choice = int(input('Select an option: '))
   return options[choice]
 
-def get_route_target():
-  steam_games = []
-  found = False
+def get_primary_executable(appid):
+  with (steam_folder / 'appcache' / 'appinfo.vdf').open('rb') as f:
+    # TODO: Look at https://github.com/SteamDatabase/SteamAppInfo
+
+
+# Search for an installed game which supports Remote Play Together
+def get_rpt_enabled_appid():
   for file in steam_folder.glob('steamapps/appmanifest_*.acf'):
-    game_data = {}
+    appid = None
     with file.open('r', encoding='utf8') as f:
       for line in f:
         if '"appid"' in line:
-          game_data['appid'] = line.split('"')[3]
-        elif '"installdir"' in line:
-          game_data['installdir'] = line.split('"')[3]
+          appid = line.split('"')[3]
+          break
 
-    text = requests.get('https://store.steampowered.com/app/' + game_data['appid']).text
+    text = requests.get('https://store.steampowered.com/app/' + appid).text
     if 'https://store.steampowered.com/remoteplay_hub' in text:
-      found = True
-      break
+      return appid
 
-  if not found:
-    print('No routable games found') # Improve this error. This means "Go download a RPT game"
+  # Else, no remote-play enabled apps are installed
+  steam_f2p_rpt_games = 'https://store.steampowered.com/search/?maxprice=free&category2=44'
+  if sys.platform == 'win32':
+    steam_f2p_rpt_games ++ '&os=win'
+  elif sys.platform == 'darwin':
+    steam_f2p_rpt_games ++ '&os=mac'
+  else:
+    steam_f2p_rpt_games ++ '&os=linux'
+  print('No available games support Steam Remote Play. Please go download a remote-play compatible game: ' + steam_f2p_rpt_games)
+  should_open = input('Open URL in Steam: Y/N')
+  if should_open == 'Y':
+    open_url('steam://openurl/' + steam_f2p_rpt_games)
+  sys.exit(0)
 
-  route_folder = steam_folder / 'steamapps' / 'common' / game_data['installdir']
-  # TODO: Non-windows is probably not 'exe'. Hmm.
-  route_target = show_chooser(list(route_folder.glob('**/*.exe')), key=lambda p: p.stem)
-  return (route_target, game_data['appid'])
 
 
 def remote_play_anything():
-  route_target, appid = get_route_target()
+  appid = get_rpt_enabled_appid()
+  route_target = get_primary_executable(appid)
 
   game_folders = [path for path in (steam_folder / 'steamapps' / 'common').iterdir() if path.is_dir()]
   game = show_chooser(game_folders, key=lambda p: p.stem)
@@ -77,34 +96,46 @@ def remote_play_anything():
   route_target.rename(route_target.parent / ('_' + route_target.name))
   shutil.copy(sys.executable, target_path)
 
+  open_url('steam://rungameid/' + appid)
+
+
+def run_target_executable():
+  with restart_file.open('r') as f:
+    exe_path = Path(f.read()) # Path to the exectuable we want to launch in our stead
+  # Remove restart file (since we have now restarted)
+  restart_file.unlink()
+
+  # Rerun as target process
+  # TODO: Remove extraneous args?
+  # subprocess.Popen([str(exe_path)], cwd=str(exe_path.parent), stdin=None, stderr=None, close_fds=None)
+
+  # Restore route target (involves deleting ourselves, so we need a bit of a hack)
+  route_target = Path(sys.executable)
+  route_target = str(route_target.parent / '_' + route_target.stem)
+  print(route_target)
+  input()
   if sys.platform == 'win32':
-    subprocess.run(['cmd', '/c', 'start', 'steam://rungameid/' + appid])
+    os.command(f'cmd /c ping 127.0.0.1 -n 4 >nul & del {sys.executable} & move {route_target} {sys.executable}') 
   elif sys.platform == 'darwin':
-    import os
-    os.command('open steam://rungameid/' + appid)
+    os.command('sleep 3; rm {sys.executable}; mv {route_target} {sys.executable}')
   else:
-    import os
-    os.command('xdg-open steam://rungameid/' + appid)
+    os.command('sleep 3; rm {sys.executable}; mv {route_target} {sys.executable}')
+    
+
+def check_for_recompile():
+  if Path(__file__).stat().st_mtime > Path(sys.executable).stat().st_mtime:
+    print('File has been modified more recently than executable, please recompile')
+    # sys.exit(0)
 
 
 if __name__ == '__main__':
-  try:
-    with restart_file.open('r') as f:
-      path = Path(f.read())
-    # Remove restart file (since we have now restarted)
-    # restart_file.unlink()
-    # Restore route target
-    # Path(sys.executable).unlink()
-    # Path('_' + sys.executable).rename(sys.executable)
-
-    subprocess.Popen([str(path)], cwd=str(path.parent), stdin=None, stderr=None, close_fds=None)
-    sys.exit(0)
-  except FileNotFoundError:
-    pass
-
-  # TODO: Compare file timestamp to exe timestamp
+  # TODO: Maybe safer: Also check if calling process is named Steam.exe
+  if restart_file.exists():
+    run_target_executable()
+  else:
+    check_for_recompile() # Only while in development
+    get_steam_folder()
+    remote_play_anything()
 
 
-  get_steam_folder()
-  remote_play_anything()
 
